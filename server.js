5198f48463b4bba752b2b38b5b3aa1f8d7ad9647
Auquier Cyril <cyrilauquier@hotmail.fr>
2023-07-07 20:55:37 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+// Define all the routes
+
+/**
+ * req == request made to the api
+ * res == response madi by the api
+ */
+app.get("/", (req, res) => {
+  res.send("Hello from NODE API");
+});
+
+app.listen(PORT, () => {
+  console.log(`Node API is running on port: ${PORT}`);
+});
